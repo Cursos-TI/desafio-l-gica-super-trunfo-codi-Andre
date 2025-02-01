2268e5830f760bad5f6e8e6f05dc480c9f40272e
André Miranda <120103103+codi-Andre@users.noreply.github.com>
2025-02-01 10:54:04 +0000

    :sparkles: feat(desafio): conclui o nível novato

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -8,7 +8,11 @@
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
-
+    unsigned int populacao1, populacao2, area1, area2, pontosTuristicos1, pontosTuristicos2;
+    long unsigned int pib1, pib2, superPoder1, superPoder2;
+    char cidade1[255], cidade2[255];
+    float densidadeDemografica1, densidadeDemografica2, pibPerCapita1, pibPerCapita2;
+    int pontuacao1 = 0, pontuacao2 = 0;
     
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
@@ -20,6 +24,72 @@ int main() {
     // scanf("%s", codigo);
     // 
     // (Repita para cada propriedade)
+    printf("Bem-vindos ao super trunfo!\nEscolha duas cartas, \nperguntas envolvendo números deve-se utilizar apenas digitos.\nBoa sorte!\n\n");
+
+    printf("Qual é a primeira cidade? ");
+    scanf("%s", &cidade1);
+    printf("\n ");
+
+    printf("Quantas pessoas vivem na primeira cidade? ");
+    scanf("%d", &populacao1);
+    printf("\n");
+
+    printf("Qual é a área em km² da primeira cidade? ");
+    scanf("%d", &area1);
+    printf("\n");
+
+    printf("Qual é o PIB da primeira cidade? ");
+    scanf("%lu", &pib1);
+    printf("\n");
+
+    printf("Quantos pontos turísticos existem na primeira cidade? ");
+    scanf("%d", &pontosTuristicos1);
+    printf("\n");
+
+    printf("Qual é a segunda cidade? ");
+    scanf("%s", &cidade2);
+    printf(" \n");
+
+    printf("Quantas pessoas vivem na segunda cidade? ");
+    scanf("%d", &populacao2);
+    printf("\n");
+
+    printf("Qual é a área em km² da segunda cidade? ");
+    scanf("%d", &area2);
+    printf("\n");
+
+    printf("Qual é o PIB da segunda cidade? ");
+    scanf("%lu", &pib2);
+    printf("\n");
+
+    printf("Quantos pontos turísticos existem na segunda cidade? ");
+    scanf("%d", &pontosTuristicos2);
+    printf("\n");
+
+    densidadeDemografica1 = (float) populacao1 / area1;
+    densidadeDemografica2 = (float) pib2 / populacao2;
+    pibPerCapita1 = (float) pib1 / populacao1;
+    pibPerCapita2 = (float) populacao2 / area2;
+    superPoder1 = populacao1 + area1 + pib1 + pontosTuristicos1 + densidadeDemografica1 + pibPerCapita1;
+    superPoder2 = populacao2 + area2 + pib2 + pontosTuristicos2 + densidadeDemografica2 + pibPerCapita2;
+
+    printf("Carta A01\n");
+    printf("Cidade: %s\n", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %d km²\n", area1);
+    printf("Pontos turísticos: %d\n", pontosTuristicos1);
+    printf("PIB: R$ %lu,00\n", pontosTuristicos1);
+    printf("Densidade demográfica: %.2f hab/km²\n", densidadeDemografica1);
+    printf("PIB per Capita: R$ %.2f \n\n\n", pibPerCapita1);
+
+    printf("Carta A02\n");
+    printf("Cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %d km²\n", area2);
+    printf("Pontos turísticos: %d\n", pontosTuristicos2);
+    printf("PIB: R$ %lu,00\n", pontosTuristicos2);
+    printf("Densidade demográfica: %.2f hab/km²\n", densidadeDemografica2);
+    printf("PIB per Capita: R$ %.2f \n\n\n", pibPerCapita2);
 
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
@@ -32,12 +102,27 @@ int main() {
     //     printf("Cidade 2 tem maior população.\n");
     // }
 
+    if (populacao1 > populacao2) {
+        pontuacao1++;
+        printf("Cidade 1 tem maior população.\n");
+    } else if (populacao1 < populacao2) {
+        pontuacao2++;
+        printf("Cidade 2 tem maior população.\n");
+    }
+
     // Exibição dos Resultados:
     // Após realizar as comparações, exiba os resultados para o usuário.
     // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
 
     // Exemplo:
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    if (pontuacao1 > pontuacao2) {
+        printf("Cidade %s venceu!\n", cidade1);
+    } else if (pontuacao1 < pontuacao2) {
+        printf("Cidade %s venceu!\n", cidade2);
+    } else {
+        printf("Empate!\n");
+    }
 
     return 0;
 }
